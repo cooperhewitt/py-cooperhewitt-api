5b0e822291fbaa25d33856feaf3b9bff61321627
straup <copea@si.edu>
2014-06-05 13:26:35 -0400

    update URLs

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(name='cooperhewitt-api',
       version='0.1',
       description='Simple Python wrapper for Cooper-Hewitt API',
       author='Smithsonian Cooper-Hewitt National Design Museum',
-      url='https://github.com/cooperhewitt/py-cooperhewitt',
+      url='https://github.com/cooperhewitt/py-cooperhewitt-api',
       requires=[],
       packages=[
           'cooperhewitt',
           'cooperhewitt.api'
       ],
       scripts=[],
-      download_url='https://github.com/cooperhewitt/py-cooperhewitt/releases/tag/v0.1',
+      download_url='https://github.com/cooperhewitt/py-cooperhewitt-api/releases/tag/v0.1',
       license='BSD')
