0b9099e8cb699352614606f31524678888d171c5
straup <straup@gmail.com>
2014-11-13 14:55:34 +0100

    re-jigger the setup file

--- cooperhewitt/__init__.py
+++ cooperhewitt/__init__.py
@@ -1,1 +1,2 @@
-# I blame, Guido
+# https://pythonhosted.org/setuptools/setuptools.html#namespace-packages
+__import__('pkg_resources').declare_namespace(__name__)
--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='cooperhewitt-api',
-      version='0.1',
-      description='Simple Python wrapper for Cooper-Hewitt API',
-      author='Smithsonian Cooper-Hewitt National Design Museum',
-      url='https://github.com/cooperhewitt/py-cooperhewitt-api',
-      requires=[],
-      packages=[
-          'cooperhewitt',
-          'cooperhewitt.api'
-      ],
-      scripts=[],
-      download_url='https://github.com/cooperhewitt/py-cooperhewitt-api/releases/tag/v0.1',
-      license='BSD')
+packages = find_packages()
+desc = open("README.md").read(),
+
+setup(
+    name='cooperhewitt.api',
+    namespace_packages=['cooperhewitt'],
+    version='0.2',
+    description='Simple Python wrapper for Cooper-Hewitt API',
+    author='Smithsonian Cooper-Hewitt National Design Museum',
+    url='https://github.com/cooperhewitt/py-cooperhewitt-api',
+    requires=[],
+    packages=packages,
+    scripts=[],
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-api/releases/tag/v0.1',
+    license='BSD')
